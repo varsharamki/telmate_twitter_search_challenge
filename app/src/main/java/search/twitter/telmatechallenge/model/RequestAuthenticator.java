f9acddd579fd4896806b02a1bec8a2e208a7066e
varsharamki <varshagautham79@gmail.com>
2017-05-10 13:34:13 -0700

    more setting up

---
+++ app/src/main/java/search/twitter/telmatechallenge/model/RequestAuthenticator.java
@@ -0,0 +1,8 @@
+package search.twitter.telmatechallenge.model;
+
+/**
+ * Created by varsha on 5/10/17.
+ */
+
+public class RequestAuthenticator {
+}
--- app/src/main/java/search/twitter/telmatechallenge/view/activities/EnterTwitterSearchActivity.java
+++ app/src/main/java/search/twitter/telmatechallenge/view/activities/EnterTwitterSearchActivity.java
@@ -1,7 +1,9 @@
-package search.twitter.telmatechallenge;
+package search.twitter.telmatechallenge.view.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import search.twitter.telmatechallenge.R;
 
 public class EnterTwitterSearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class EnterTwitterSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_twitter_search);
+        /*
+        get the ba
+         */
     }
 }
